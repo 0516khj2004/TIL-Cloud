45c62092b5c019e286648467a0ab122337f85297
0516khj2004 <0516khj2004@naver.com>
2020-04-20 22:41:08 +0900

    4/20 restfull api

---
+++ JAVA/MySpring4Project/src/myspring/user/conroller/RestUserController.java
@@ -0,0 +1,67 @@
+package myspring.user.conroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import myspring.user.service.UserService;
+import myspring.user.vo.UserVO;
+
+@RestController
+//controller + responseBody
+
+public class RestUserController {
+	@Autowired
+	UserService userService;
+	
+	//사용자 목록 
+	@GetMapping("/users")
+	public List<UserVO> userList(){
+		return userService.getUserList();
+	}
+	
+	//사용자 상세 조회
+	@GetMapping("/users/{id}")
+	public UserVO userDetail(@PathVariable("id") String userid) { // @PathVariable String id {id}와 변수이름을 맞추면 variable안써도 된다
+		
+		return userService.getUser(userid);
+	}
+	//사용자 등록 
+	@PostMapping("/users")
+	public Boolean userInsert(@RequestBody UserVO user) {
+		if(user != null) {
+			userService.insertUser(user);
+			return Boolean.TRUE;
+		}else {
+			return Boolean.FALSE;
+		}
+	}
+	
+	//사용자 수정 
+	@PutMapping("/users")
+	public Boolean userUpdate(@RequestBody UserVO user) {
+		if(user != null) {
+			userService.updateUser(user);
+			return Boolean.TRUE;
+		}else {
+			return Boolean.FALSE;
+		}
+	}
+	@DeleteMapping("/users/{id}")
+	public Boolean userDBoolean(@PathVariable String id) {
+		if(id != null) {
+			userService.deleteUser(id);
+			return Boolean.TRUE;
+		}else {
+			return Boolean.FALSE;
+		}
+	}
+}
--- JAVA/MySpring4Project/src/myspring/user/conroller/UserController.java
+++ JAVA/MySpring4Project/src/myspring/user/conroller/UserController.java
@@ -1,11 +1,18 @@
 package myspring.user.conroller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,14 +28,83 @@ public class UserController {
 	@RequestMapping("/userList.do")
 	public ModelAndView userList() {
 		List<UserVO> userList = userService.getUserList();
-		return new ModelAndView("userList.jsp", "userList", userList); // = request.setAttribute("userList", users);
+		return new ModelAndView("userList", "userList", userList); // = request.setAttribute("userList", users);
 	
 	}
 	//사용자 상제정보 조회
 	@RequestMapping("/userDetail.do")
+	//@RequestParam => request.getParameter()
+	//QueryString ? key1=value&key2=value2
 	public String userDetail(@RequestParam String userid, Model model ) { // @RequestParam = request.getParameter("id")
 		UserVO user = userService.getUser(userid);
 		model.addAttribute("user", user);
-		return "userDetail.jsp";
+		return "userDetail";
 	}
+	//사용자 등록 Form 조회 
+	@RequestMapping("/userInsertForm.do")
+	public String insertUserForm(HttpSession session) {
+		List<String> genderList = new ArrayList<String>();
+		genderList.add("남");
+		genderList.add("여");
+		//session에 genderList저장 
+		session.setAttribute("genderList", genderList);
+
+		List<String> cityList = new ArrayList<String>();
+		cityList.add("서울");
+		cityList.add("부산");
+		cityList.add("대구");
+		cityList.add("제주");
+		cityList.add("인천");
+		//session에 cityList저장
+		session.setAttribute("cityList", cityList);
+
+		return "userInsert";
+	}
+	//사용자 등록 처리 
+	@RequestMapping(value = "/userInsert.do", method = RequestMethod.POST)
+	public String usertInsert(@ModelAttribute UserVO user) {
+		System.out.println(">> UserVo" + user);
+		userService.insertUser(user);
+		
+		//사용자 목록조회를 처리하는 요청으로 포워딩  하겠다
+		return "redirect:/userList.do";
+	}
+	
+	//사용자 삭제처리 -> userDelete.do/gildog(userid) url / base으로 append하는 방식  @PathVariable
+	@RequestMapping("/userDelete.do/{id}")
+	public String userDelete(@PathVariable("id") String userid) {
+		userService.deleteUser(userid);
+		return "redirect:/userList.do";
+	
+	}
+
+	//사용자 updateform
+	   @RequestMapping("/updateUserForm.do")
+	   public String updateUserForm(HttpSession session, @RequestParam String id) {
+	      UserVO user = userService.getUser(id);
+
+	      List<String> genderList = new ArrayList<String>();
+	      genderList.add("남");
+	      genderList.add("여");
+	      //session객체 genderList 객체를 저장
+	      session.setAttribute("genderList", genderList);
+
+	      List<String> cityList = new ArrayList<String>();
+	      cityList.add("서울");
+	      cityList.add("경기");
+	      cityList.add("부산");
+	      cityList.add("대구");
+	      cityList.add("제주");
+	      //session객체에 cityList 객체를 저장
+	      session.setAttribute("cityList", cityList);
+	      session.setAttribute("user", user);
+	      return "userUpdate";
+	   }
+
+	   //사용자 update처리
+	   @RequestMapping("/userUpdate.do")
+	   public String userUpdate(@ModelAttribute UserVO user) {
+	      userService.updateUser(user);
+	      return "redirect:/userList.do";
+	   }
 }
